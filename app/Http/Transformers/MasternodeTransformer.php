4666ff0bc0817a71198ad692ddc263e2ae9a96ae
pxgamer <owzie123@gmail.com>
2018-12-18 15:37:51 +0000

    Add Masternode transformer class

---
+++ app/Http/Transformers/MasternodeTransformer.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Transformers;
+
+use App\Masternode;
+use League\Fractal\TransformerAbstract;
+
+/**
+ * Class MasternodeTransformer
+ */
+class MasternodeTransformer extends TransformerAbstract
+{
+    /**
+     * @param Masternode $masternode
+     * @return array
+     */
+    public function transform(Masternode $masternode): array
+    {
+        return [
+            'id' => $masternode->public_key,
+            'height' => $masternode->height,
+            'ip' => $masternode->ip,
+            'last_won' => $masternode->last_won,
+            'blacklist' => $masternode->blacklist,
+            'fails' => $masternode->fails,
+            'status' => $masternode->status,
+        ];
+    }
+}
