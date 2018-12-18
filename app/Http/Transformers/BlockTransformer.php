fabbf78e680aba6e12250e6a0a0f797e4c2ecb09
pxgamer <owzie123@gmail.com>
2018-12-18 15:33:02 +0000

    Add Block transformer class

---
+++ app/Http/Transformers/BlockTransformer.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Transformers;
+
+use App\Block;
+use League\Fractal\TransformerAbstract;
+
+/**
+ * Class BlockTransformer
+ */
+class BlockTransformer extends TransformerAbstract
+{
+    /**
+     * @param Block $block
+     * @return array
+     */
+    public function transform(Block $block): array
+    {
+        return [
+            'id' => $block->id,
+            'generator' => $block->generator,
+            'height' => $block->height,
+            'date' => $block->date,
+            'nonce' => $block->nonce,
+            'signature' => $block->signature,
+            'difficulty' => $block->difficulty,
+            'argon' => $block->argon,
+            'transactions' => $block->transactions,
+        ];
+    }
+}
