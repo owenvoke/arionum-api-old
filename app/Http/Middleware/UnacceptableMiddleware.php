e9624ef21971596483460b95472c88ed2b3bd40a
pxgamer <owzie123@gmail.com>
2019-01-31 15:36:06 +0000

    Import class for usage in middleware

--- app/Http/Middleware/UnacceptableMiddleware.php
+++ app/Http/Middleware/UnacceptableMiddleware.php
@@ -3,16 +3,14 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Http\Request;
 use Symfony\Component\HttpKernel\Exception\NotAcceptableHttpException;
 
-/**
- * Class UnacceptableMiddleware
- */
 class UnacceptableMiddleware
 {
     /**
-     * @param \Illuminate\Http\Request $request
-     * @param Closure                  $next
+     * @param Request $request
+     * @param Closure $next
      * @return mixed
      */
     public function handle($request, Closure $next)
