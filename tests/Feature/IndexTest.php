2196b9f687f1d329c90192071d64908cc14b53fb
pxgamer <owzie123@gmail.com>
2019-01-31 17:21:12 +0000

    Remove redundant PHPUnit feature tests

--- tests/Feature/IndexTest.php
+++ tests/Feature/IndexTest.php
@@ -4,48 +4,13 @@ namespace Tests\Feature;
 
 use Tests\TestCase;
 
-/**
- * Class IndexTest
- */
 class IndexTest extends TestCase
 {
-    /** @var string */
-    private const JSON_MIME_TYPE = 'application/json';
-
-    /**
-     * @test
-     * @return void
-     */
-    public function indexWithOnlyContentTypeAccepted(): void
+    /** @test */
+    public function itCanRetrieveTheIndexWithJsonHeaders(): void
     {
-        $this->get('/', [
-            'Content-Type' => self::JSON_MIME_TYPE,
-        ])->seeJsonEquals(['message' => 'You must accept JSON', 'status' => 406]);
-    }
-
-    /**
-     * @test
-     * @return void
-     */
-    public function indexWithOnlyJsonAccepted(): void
-    {
-        $this->get('/', [
-            'Accept' => self::JSON_MIME_TYPE,
-        ])->seeJsonEquals(['message' => 'Unsupported Media Type', 'status' => 415]);
-    }
-
-    /**
-     * @test
-     * @return void
-     */
-    public function indexWithJsonHeaders(): void
-    {
-        $this->json('GET', '/')->seeJsonEquals([
-            'accounts_url' => route('accounts'),
-            'blocks_url' => route('blocks'),
-            'masternodes_url' => route('masternodes'),
-            'mempools_url' => route('mempools'),
-            'transactions_url' => route('transactions'),
+        $this->json('GET', '/')->seeJsonStructure([
+            'routes' => [],
         ]);
     }
 }
