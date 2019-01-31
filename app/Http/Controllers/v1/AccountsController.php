80b9dad015b41993fa0f695d12d140623c5aae17
pxgamer <owzie123@gmail.com>
2019-01-31 15:20:23 +0000

    Move current API routes to a v1 directory

--- app/Http/Controllers/v1/AccountsController.php
+++ app/Http/Controllers/v1/AccountsController.php
@@ -1,20 +1,14 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\v1;
 
 use App\Account;
+use App\Http\Controllers\Controller;
 use App\Http\Transformers\AccountTransformer;
 use Illuminate\Http\JsonResponse;
 
-/**
- * Class AccountsController
- */
 class AccountsController extends Controller
 {
-    /**
-     * @param string|null $id
-     * @return JsonResponse
-     */
     public function list(?string $id = null): JsonResponse
     {
         $data = $id ? Account::findOrFail($id) : Account::query()->paginate();
