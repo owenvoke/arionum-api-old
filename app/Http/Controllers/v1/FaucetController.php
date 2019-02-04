7e1d620de2c559f6e5eda1eac142402c565e0a7e
pxgamer <owzie123@gmail.com>
2019-02-04 15:57:46 +0000

    Add Faucet controller for the Faucet API

---
+++ app/Http/Controllers/v1/FaucetController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\v1;
+
+use App\Account;
+use App\Http\Controllers\Controller;
+use App\Http\Transformers\FaucetTransformer;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class FaucetController extends Controller
+{
+    public const ADDRESS = '2bG9Z7LiMaCoz6WhcnR55z8Uz73RdmLDyn62bWTovuPLK4x6ThYyWGaUKL5HKBHE3UUzsjNJk8He9BNKFd5Zr93Q';
+
+    public function index(Request $request): JsonResponse
+    {
+        $response = fractal(Account::query()->find(self::ADDRESS), FaucetTransformer::class);
+
+        if ($request->get('withTransactions')) {
+            $response = $response->includeTransactions();
+        }
+
+        return $response->respond();
+    }
+
+    public function balance(): array
+    {
+        return [
+            'data' => [
+                'balance' => Account::query()->find(self::ADDRESS)->balance,
+            ],
+        ];
+    }
+}
