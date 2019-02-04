1202a0d3116b1d0c01b1e281420a0d1fc96fcf8c
pxgamer <owzie123@gmail.com>
2019-02-04 15:44:49 +0000

    Add Account methods for transactions

--- app/Account.php
+++ app/Account.php
@@ -2,16 +2,21 @@
 
 namespace App;
 
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 /**
  * Class Account
  *
- * @property-read string $id
- * @property-read string $public_key
- * @property-read string $block
- * @property-read float  $balance
- * @property-read string $alias
+ * @property-read string                 $id
+ * @property-read string                 $public_key
+ * @property-read string                 $block
+ * @property-read float                  $balance
+ * @property-read string                 $alias
+ *
+ * @property-read Collection|Transaction $transactionsFrom
+ * @property-read Collection|Transaction $transactionsTi
  */
 class Account extends Model
 {
@@ -22,4 +27,14 @@ class Account extends Model
     protected $casts = [
         'balance' => 'float',
     ];
+
+    public function transactionsFrom(): HasMany
+    {
+        return $this->hasMany(Transaction::class, 'public_key', 'public_key');
+    }
+
+    public function transactionsTo(): HasMany
+    {
+        return $this->hasMany(Transaction::class, 'dst');
+    }
 }
