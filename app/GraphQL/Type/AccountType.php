8ad1d97d4aad1d8201b8e20cf0c15cddad1811db
pxgamer <owzie123@gmail.com>
2019-04-11 13:00:35 +0100

    Add a GraphQL Account type

---
+++ app/GraphQL/Type/AccountType.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\GraphQL\Type;
+
+use App\Account;
+use GraphQL\Type\Definition\Type;
+use Rebing\GraphQL\Support\Type as GraphQLType;
+
+final class AccountType extends GraphQLType
+{
+    protected $attributes = [
+        'name' => 'Account',
+        'description' => 'An account',
+        'model' => Account::class,
+    ];
+
+    public function fields(): array
+    {
+        return [
+
+            'id' => [
+                'type' => Type::nonNull(Type::id()),
+                'description' => 'The id or address of the account',
+            ],
+
+            'public_key' => [
+                'type' => Type::string(),
+                'description' => 'The public key registered with the account',
+            ],
+
+            'block' => [
+                'type' => Type::nonNull(Type::string()),
+                'description' => 'The block id that the account was created on',
+            ],
+
+            'balance' => [
+                'type' => Type::nonNull(Type::float()),
+                'description' => 'The current balance of the account',
+            ],
+
+            'alias' => [
+                'type' => Type::string(),
+                'description' => 'The alias registered with the account',
+            ],
+
+        ];
+    }
+}
