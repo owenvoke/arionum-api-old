39e625511e83b4576b6ce0bf0c7cb0cc76def92b
pxgamer <owzie123@gmail.com>
2019-04-11 13:02:09 +0100

    Add a GraphQL Account query

---
+++ app/GraphQL/Query/AccountQuery.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\GraphQL\Query;
+
+use App\Account;
+use GraphQL\Type\Definition\Type;
+use Rebing\GraphQL\Support\Facades\GraphQL;
+use Rebing\GraphQL\Support\Query;
+
+final class AccountQuery extends Query
+{
+    protected $attributes = [
+        'name' => 'Account query',
+    ];
+
+    public function type()
+    {
+        return GraphQL::paginate('accounts');
+    }
+
+    public function args(): array
+    {
+        return [
+            'id' => ['name' => 'id', 'type' => Type::id()],
+            'alias' => ['name' => 'alias', 'type' => Type::string()],
+        ];
+    }
+
+    public function resolve($root, $args)
+    {
+        if (isset($args['id'])) {
+            return Account::query()->find($args['id']);
+        }
+
+        if (isset($args['alias'])) {
+            return Account::findByAlias($args['alias']);
+        }
+
+        return Account::query()->paginate($args['limit'], ['*'], 'page', $args['page']);
+    }
+}
