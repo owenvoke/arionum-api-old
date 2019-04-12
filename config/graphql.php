e2cd4c8c5b484354bc9c2ac3ceca48bb36160a02
pxgamer <owzie123@gmail.com>
2019-04-12 14:36:43 +0100

    Add the query and type to the configuration

--- config/graphql.php
+++ config/graphql.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\GraphQL\Controllers\LumenGraphQLController;
+use App\GraphQL\Query\MempoolQuery;
+use App\GraphQL\Query\MempoolsQuery;
+use App\GraphQL\Type\MempoolType;
 use App\Http\Middleware\PreconditionHeaderMiddleware;
 use Rebing\GraphQL\GraphQL;
 use Rebing\GraphQL\Support\PaginationType;
@@ -44,7 +47,10 @@ return [
 
     'schemas' => [
         'default' => [
-            'query' => [],
+            'query' => [
+                'mempool' => MempoolQuery::class,
+                'mempools' => MempoolsQuery::class,
+            ],
             'mutation' => [],
             'middleware' => [],
             'method' => ['post'],
@@ -54,7 +60,9 @@ return [
     // The types available in the application. You can then access it from the facade like this:
     // GraphQL::type('user')
 
-    'types' => [],
+    'types' => [
+        'Mempool' => MempoolType::class,
+    ],
 
     // This callable will be passed the Error object for each errors GraphQL catch.
     // The method should return an array representing the error.
